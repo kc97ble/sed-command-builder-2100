e52842bcd8e81f3af628ea258b95b46bee870ffb
Kien Nguyen <kc97ble@gmail.com>
2022-09-11 21:27:31 +0700

    Install jQuery

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello!");
+import * as $ from "jquery";
+
+console.log("Welcome to sed-command-builder!");
+$("body").css("background-color", "#fcfcfc");
+(window as any).jquery = $; // for debugging purpose only
