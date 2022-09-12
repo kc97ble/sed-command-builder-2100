e6399eb4998c069afa0efbf1c91813c8b51757ee
Kien Nguyen <kc97ble@gmail.com>
2022-09-12 11:26:42 +0700

    Edit logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,55 @@
 import * as $ from "jquery";
+import { build } from "./utils/builder";
+import { FormData } from "./types";
 
 console.log("Welcome to sed-command-builder!");
 $("body").css("background-color", "#fcfcfc");
 (window as any).jquery = $; // for debugging purpose only
+
+const formData: FormData = {
+  condition: "all",
+  command: "none",
+};
+
+type BuildResult = { ok: true; result: string } | { ok: false; message: string };
+
+function getBuildResult(): BuildResult {
+  try {
+    const result = build(formData);
+    return { ok: true, result };
+  } catch (e) {
+    const message = e instanceof Error ? e.message : JSON.stringify(e);
+    return { ok: false, message };
+  }
+}
+
+function render() {
+  for (const name in formData) {
+    $(`input[type=text][name=${name}]`).val(formData[name]);
+    $(`input[type=number][name=${name}]`).val(formData[name]);
+    $(`input[type=radio][name=${name}]`).val([formData[name]]);
+  }
+  const buildResult = getBuildResult();
+  if (buildResult.ok) {
+    $("#result-0949").text(buildResult.result).css("color", "inherit");
+  } else {
+    $("#result-0949").text(buildResult.message).css("color", "red");
+  }
+}
+
+function setup() {
+  $("body")
+    .find("input")
+    .each((_, inputElement) => {
+      $(inputElement).on("input", () => {
+        const { name, value } = inputElement;
+        formData[name] = value;
+        render();
+      });
+    });
+}
+
+window.onload = () => {
+  setup();
+  render();
+};
